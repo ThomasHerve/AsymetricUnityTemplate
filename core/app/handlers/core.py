439b13725df7b15ce72626a04c8198b9404d054b
Thomas <therve38@gmail.com>
2024-03-28 19:54:47 +0100

    Create pod on demand

--- core/app/handlers/core.py
+++ core/app/handlers/core.py
@@ -1,15 +1,29 @@
-import hug
+import hug, os, random, string
 from kubernetes import client, config
 
 @hug.post('/create-room')
 def create_room():
+
+    image = os.environ["INSTANCE_IMAGE"]
+    namespace = os.environ["KUBERNETES_NAMESPACE"]
+
+    # Random pod id
+    pod_id = ''.join(random.choice(string.ascii_lowercase) for i in range(10)) 
+
     config.load_kube_config(config_file='./kubernetes-config')
     v1 = client.CoreV1Api()
-    l = []
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        l.append(i.status.pod_ip + " " + i.metadata.namespace + " " + i.metadata.name)
-    return l
+	
+    containers = []
+    container1 = client.V1Container(name='instance', image=image)
+    containers.append(container1)
+
+    pod_spec = client.V1PodSpec(containers=containers)
+    pod_metadata = client.V1ObjectMeta(name='instance-' + pod_id, namespace=namespace)
+
+    pod_body = client.V1Pod(api_version='v1', kind='Pod', metadata=pod_metadata, spec=pod_spec)
+        
+    v1.create_namespaced_pod(namespace='default', body=pod_body)
+
 
 @hug.post('/delete-room')
 def delete_room(body):
