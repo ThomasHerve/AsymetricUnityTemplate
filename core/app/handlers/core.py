8d2e6decbf5d920a0bf6a14e1637fbfaf38db2ab
Thomas <therve38@gmail.com>
2024-03-28 18:47:04 +0100

    Check i'm getting the token

--- core/app/handlers/core.py
+++ core/app/handlers/core.py
@@ -2,7 +2,10 @@ import hug
 
 @hug.post('/create-room')
 def create_room():
-    return ""
+    token = ""
+    with open("kubernetes-token/token", "r") as f:
+        token = f.read()
+    return token
 
 @hug.post('/delete-room')
 def delete_room(body):
