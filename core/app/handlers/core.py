765b1c0da74d78225b9ba5d2a5dc98e3ca435428
Thomas HERVE <therve@descours-cabaud.net>
2024-03-30 15:42:52 +0100

    patch ingress on instance creation

--- core/app/handlers/core.py
+++ core/app/handlers/core.py
@@ -6,6 +6,8 @@ def create_room():
 
     image = os.environ["INSTANCE_IMAGE"]
     namespace = os.environ["KUBERNETES_NAMESPACE"]
+    ingress = os.environ["KUBERNETES_INGRESS_NAME"]
+    port = os.environ["KUBERNETES_PORT"]
 
     # Random pod id
     pod_id = ''.join(random.choice(string.ascii_lowercase) for i in range(10)) 
@@ -28,7 +30,7 @@ def create_room():
     v1.create_namespaced_pod(namespace=namespace , body=pod_body)
 
     # Create a service
-    service_port_list = [client.V1ServicePort(port=8000, target_port=8000, name='http')]
+    service_port_list = [client.V1ServicePort(port=port, target_port=port, name='http')]
     service_spec = client.V1ServiceSpec(ports=service_port_list, selector={
         "pod_id": pod_id
     })
@@ -36,6 +38,16 @@ def create_room():
     service = client.V1Service(metadata=service_metadata, spec=service_spec)
     
     v1.create_namespaced_service(namespace=namespace , body=service)
+
+    # Update the ingress class
+    networking = client.NetworkingV1Api()
+
+    current_ingress = networking.read_namespaced_ingress(name=ingress, namespace=namespace)
+    current_ingress_paths = current_ingress.spec.rules[0].http.paths
+    ingress_paths = current_ingress_paths.append(client.V1HTTPIngressPath(path=f"/{pod_id}(/|$)(.*)", path_type="Prefix", backend=client.V1IngressBackend(service=client.V1IngressServiceBackend(name=f"instance-{pod_id}", port=client.V1ServiceBackendPort(number=port)))))
+    current_ingress.spec.rules[0].http.paths = ingress_paths
+
+    networking.patch_namespaced_ingress(ingress, namespace, current_ingress)
 
     return pod_id
 
