7ca61795d26cb510585616ab5e7ac22340c42a0a
Thomas HERVE <therve@descours-cabaud.net>
2024-03-30 16:18:25 +0100

    Fix

--- core/app/handlers/core.py
+++ core/app/handlers/core.py
@@ -71,7 +71,7 @@ def delete_room(body):
     networking = client.NetworkingV1Api()
     current_ingress = networking.read_namespaced_ingress(name=ingress, namespace=namespace)
     current_ingress_paths = current_ingress.spec.rules[0].http.paths
-    current_ingress_paths = list(filter(lambda x: x.backend.name != f"instance-{body["instance"]}", current_ingress_paths))
+    current_ingress_paths = list(filter(lambda x: x.backend.service.name != f"instance-{body["instance"]}", current_ingress_paths))
     current_ingress.spec.rules[0].http.paths = current_ingress_paths
 
     networking.patch_namespaced_ingress(ingress, namespace, current_ingress)
