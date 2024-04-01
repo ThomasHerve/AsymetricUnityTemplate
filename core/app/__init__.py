424e1de57c3f23bf51afa0638b64ba5465f06a03
Thomas <therve38@gmail.com>
2024-04-01 21:27:01 +0200

    Add CORS middleware

--- core/app/__init__.py
+++ core/app/__init__.py
@@ -1,5 +1,9 @@
 import hug
 from handlers import test, core
+from hug_middleware_cors import CORSMiddleware
+
+api = hug.API(__name__)
+api.http.add_middleware(CORSMiddleware(api))
 
 
 @hug.extend_api("")
