56a837200aca92a357d41f410416c98f345af335
Thomas HERVE <therve@descours-cabaud.net>
2024-04-06 12:29:50 +0200

    add cooldown

--- frontend/src/app/asymetric-unity-template/asymetric-unity-template.component.ts
+++ frontend/src/app/asymetric-unity-template/asymetric-unity-template.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UrlService } from '../url.service';
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-asymetric-unity-template',
@@ -10,6 +12,8 @@ import { UrlService } from '../url.service';
 export class AsymetricUnityTemplateComponent implements OnInit {
 
   variable: string | undefined;
+  cooldown: number = 5;
+  currentCooldown: number = 5;
 
   constructor(private route: ActivatedRoute, private urlService: UrlService) {
   }
@@ -18,10 +22,55 @@ export class AsymetricUnityTemplateComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.variable = params['variable'];
     });
+    this.animation("0");
+    this.animation("1");
+    this.animation("2");
+    this.animation("3");
+
+    // Cooldown
+    interval(100)
+    .subscribe(() => {
+      if(this.currentCooldown < this.cooldown) {
+        this.currentCooldown += 0.1;
+        if(this.currentCooldown > this.cooldown) {
+          this.currentCooldown = this.cooldown;
+          document.getElementById("0")!.style.backgroundColor = "#1a237e"
+          document.getElementById("1")!.style.backgroundColor = "#1a237e"
+          document.getElementById("2")!.style.backgroundColor = "#1a237e"
+          document.getElementById("3")!.style.backgroundColor = "#1a237e"
+          document.getElementById("0")!.style.filter = "grayscale(0%)"
+          document.getElementById("1")!.style.filter = "grayscale(0%)"
+          document.getElementById("2")!.style.filter = "grayscale(0%)"
+          document.getElementById("3")!.style.filter = "grayscale(0%)"
+        }
+      }
+    });
+
   }
 
+  animation(value: string) {
+    const animated = document.getElementById(value);
+
+    animated!.addEventListener("animationend", () => {
+      animated?.classList.remove('anim');
+    });
+  }
 
   click(value: string) {
-    this.urlService.publish(this.variable, value).subscribe((x)=>console.log)
+    if(this.currentCooldown == this.cooldown) {
+      console.log("TEST")
+      this.currentCooldown = 0;
+      document.getElementById("0")!.style.backgroundColor = "grey"
+      document.getElementById("1")!.style.backgroundColor = "grey"
+      document.getElementById("2")!.style.backgroundColor = "grey"
+      document.getElementById("3")!.style.backgroundColor = "grey"
+      document.getElementById("0")!.style.filter = "grayscale(100%)"
+      document.getElementById("1")!.style.filter = "grayscale(100%)"
+      document.getElementById("2")!.style.filter = "grayscale(100%)"
+      document.getElementById("3")!.style.filter = "grayscale(100%)"
+      const animated = document.getElementById(value);
+      animated?.classList.add('anim');
+      this.urlService.publish(this.variable, value).subscribe((x)=>console.log)  
+    }
   }
 }
